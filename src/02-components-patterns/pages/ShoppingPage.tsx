dda227f39fc1b28366720a1b75a2e7518070d02c
JulioAvalos <julioavalos2@msn.com>
2022-01-14 16:58:42 -0600

    ajustando las interfaces faltantes

--- src/02-components-patterns/pages/ShoppingPage.tsx
+++ src/02-components-patterns/pages/ShoppingPage.tsx
@@ -1,16 +1,21 @@
-import { ProductButtons, ProductCard, ProductImage, ProductTitle } from '../components';
+import {
+  ProductButtons,
+  ProductCard,
+  ProductImage,
+  ProductTitle,
+} from '../components';
 
 import '../styles/custom-styles.css';
 
 const product = {
   id: '1',
   title: 'Coffee Mug - Card',
-  img: './coffee-mug.png'
+  img: './coffee-mug.png',
 };
 
 export const ShoppingPage = () => {
   return (
-    <div >
+    <div>
       <h1>Shopping Store</h1>
       <hr />
       <div
@@ -21,18 +26,15 @@ export const ShoppingPage = () => {
         }}
       >
         <ProductCard product={product}>
-          <ProductCard.Image />
-          <ProductCard.Title title="Hola Mundo" />
-          <ProductCard.Buttons  /> 
+          <ProductCard.Image className='custom-image' />
+          <ProductCard.Title title='Hola Mundo' className='text-bold' />
+          <ProductCard.Buttons className='custom-buttons' />
         </ProductCard>
 
-        <ProductCard 
-          product={product} 
-          className="bg-dark text-white "
-        >
-          <ProductImage className="custom-image" />
-          <ProductTitle className="text-bold"/>
-          <ProductButtons className="custom-buttons" /> 
+        <ProductCard product={product} className='bg-dark text-white'>
+          <ProductImage className='custom-image' />
+          <ProductTitle className='text-bold' />
+          <ProductButtons className='custom-buttons' />
         </ProductCard>
       </div>
     </div>

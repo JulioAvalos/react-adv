ab2c53d2960351250515c39aaa959f9659855f34
JulioAvalos <julioavalos2@msn.com>
2022-01-24 18:11:10 -0600

    refactorizacion

--- src/02-components-patterns/hooks/useShoppingCart.ts
+++ src/02-components-patterns/hooks/useShoppingCart.ts
@@ -7,30 +7,22 @@ export const useShoppingCart = () => {
     [key: string]: ProductInCart;
   }>({});
 
-  const onProductCountChange = ({
-    count,
-    product,
-  }: {
-    count: number;
-    product: Product;
-  }) => {
-    setShoppingCart((oldShoppingCart) => {
-      const productInCart: ProductInCart = oldShoppingCart[product.id] || {
-        ...product,
-        count: 0,
-      };
+  const onProductCountChange = ({ count, product }: { count: number, product: Product }) => {
 
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
-        return {
-          ...oldShoppingCart,
-          [product.id]: productInCart,
-        };
+    setShoppingCart((oldShoppingCart) => {
+      
+      if(count === 0) {
+        const { [product.id]:toDelete, ...rest} = oldShoppingCart
+        return rest;
       }
 
-      const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-      return rest;
+      return {
+        ...oldShoppingCart,
+        [product.id]: {...product, count}
+      }
+
     });
+
   };
 
   return {

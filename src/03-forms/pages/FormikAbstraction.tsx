02dafe25447e152f3debdd45375db4a7a28f8e62
JulioAvalos <julioavalos2@msn.com>
2022-03-05 16:28:48 -0600

    Agregando custom checkbox

--- src/03-forms/pages/FormikAbstraction.tsx
+++ src/03-forms/pages/FormikAbstraction.tsx
@@ -1,10 +1,11 @@
-import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 
 import { MyTextInput } from '../components/MyTextInput';
 import { MySelect } from '../components/MySelect';
 
 import '../styles/styles.css';
+import { MyCheckbox } from '../components/MyCheckbox';
 
 export const FormikAbstraction = () => {
   return (
@@ -64,11 +65,7 @@ export const FormikAbstraction = () => {
               <option value="it-jr" >IT Jr.</option>
             </MySelect>
 
-            <label>
-              <Field name="terms" type="checkbox" />
-              Terms and conditions
-            </label>
-            <ErrorMessage name='terms' component="span"/>
+            <MyCheckbox label='Terms and conditions' name='terms' />
 
             <button type='submit'>Submit</button>
           </Form>

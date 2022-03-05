8d155ed2c8941459757ccb644c0e79b4c6fa2b6d
JulioAvalos <julioavalos2@msn.com>
2022-03-05 16:22:48 -0600

    Agregando un custom select

--- src/03-forms/pages/FormikAbstraction.tsx
+++ src/03-forms/pages/FormikAbstraction.tsx
@@ -2,6 +2,7 @@ import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 import { MyTextInput } from '../components/MyTextInput';
+import { MySelect } from '../components/MySelect';
 
 import '../styles/styles.css';
 
@@ -55,15 +56,13 @@ export const FormikAbstraction = () => {
               type="email"
             />
 
-            <label htmlFor='jobType'>Job Type</label>
-            <Field name="jobType" as="select">
+            <MySelect label='Job Type' name="jobType" as="select">
               <option value="">Pick something</option>
               <option value="developer">Developer</option>
               <option value="designer">Designer</option>
               <option value="it-senior">IT Senior</option>
               <option value="it-jr" >IT Jr.</option>
-            </Field>
-            <ErrorMessage name='jobType' component="span"/>
+            </MySelect>
 
             <label>
               <Field name="terms" type="checkbox" />
